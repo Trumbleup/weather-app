b93c39cef63db5fd346df1d35bc8959235756fac
Danny <Dtrumble12@gmail.com>
2021-06-11 13:03:39 -0500

    Updates

---
+++ precache-manifest.b6bbcbe13e5176fa93757b21c19735f5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12b052dbfd0f2ed76bf66efdf040446",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5d142abcc06047d2b277",
+    "url": "/weather-app/static/css/main.549892ea.chunk.css"
+  },
+  {
+    "revision": "0d7e663c2803804d3f3f",
+    "url": "/weather-app/static/js/2.b561443c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather-app/static/js/2.b561443c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d142abcc06047d2b277",
+    "url": "/weather-app/static/js/main.8d29a08b.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "440a43c9afaa62b97c5b87864983ee8a",
+    "url": "/weather-app/static/media/raleway-regular-webfont.440a43c9.woff2"
+  },
+  {
+    "revision": "4b303357970a1bdabcedcf8358ef9b5e",
+    "url": "/weather-app/static/media/raleway-regular-webfont.4b303357.woff"
+  },
+  {
+    "revision": "dc2450fb6e748e25caca5288c051f617",
+    "url": "/weather-app/static/media/snesitalic-1g9be-webfont.dc2450fb.woff"
+  },
+  {
+    "revision": "f725b7a8247de065d4223a4555b2a367",
+    "url": "/weather-app/static/media/snesitalic-1g9be-webfont.f725b7a8.woff2"
+  }
+]);
